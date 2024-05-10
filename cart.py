052fc65d57add2d7dac3bb7f1811f3f171325e97
Sophia Alexander <32229134+Yxxsung@users.noreply.github.com>
2024-05-09 20:40:51 -0400

    first update

---
+++ cart.py
@@ -0,0 +1,12 @@
+#credit for most of the code goes to geeksforgeeks.org 
+#thanks to their tutorial: https://www.geeksforgeeks.org/how-to-add-cart-in-a-web-page-using-django/
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticgiles',
+    'cart'
+]
