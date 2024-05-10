d10aa0e694586ec1077f57d963fc028b8d86325f
Sophia Alexander <32229134+Yxxsung@users.noreply.github.com>
2024-05-10 19:38:23 -0400

    Final tutorial update

--- admin.py
+++ admin.py
@@ -3,3 +3,6 @@ from .models import Product, CartItem
  
 admin.site.register(Product)
 admin.site.register(CartItem)
+
+#Now, Go to the http://127.0.0.1:8000/admin/ and add the Images, name and its description.
+#First we need to figure out how to log into the admin login prompt
