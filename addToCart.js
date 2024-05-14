83892a3571967c8193b99779adff10c91534a79f
Sophia Alexander <32229134+Yxxsung@users.noreply.github.com>
2024-05-13 22:41:15 -0400

    more updates

--- addToCart.js
+++ addToCart.js
@@ -1,6 +1,7 @@
 
-var addItemId = 0;
 
+
+var addItemId = 0;
 function addToCart(item)
 {
     addItemId += 1;
@@ -14,8 +15,21 @@ function addToCart(item)
     var label = document.createElement('div');
     label.innerText = item.children[2].children[0].innerText;
     var select = document.createElement('span');
+    select.innerText = item.children[2].children[1].value;
+    lebel.append(select);
+    var delBtn = document.createElement('button');
+    delBtn.innerText = 'Delete';
+    delBtn.setAttribute('onclick','del('+addItemID+')');
     var cartItems = document.getElementByID('title');
     selectedItem.append(img);
+    selectedItem.append(title);
+    selectedItem.append(label);
+    selectedItem.append(delBtn);
     cartItems.append(selectedItem);
+}
+
+function del(item)
+{
+    document.getElementById(item).remove();
 }
 
