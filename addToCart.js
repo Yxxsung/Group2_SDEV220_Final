10f621b78284e3b5291394bcd0e16793708f4365
Sophia Alexander <32229134+Yxxsung@users.noreply.github.com>
2024-05-13 22:31:25 -0400

    more updates

--- addToCart.js
+++ addToCart.js
@@ -9,6 +9,11 @@ function addToCart(item)
     selectedItem.setAttribute('id',addItemId);
     var img = document.createElement('img');
     img.setAttribute('src', item.children[0].currentSrc);
+    var title = document.createElement('div');
+    title.innerText = item.children[1].innerText;
+    var label = document.createElement('div');
+    label.innerText = item.children[2].children[0].innerText;
+    var select = document.createElement('span');
     var cartItems = document.getElementByID('title');
     selectedItem.append(img);
     cartItems.append(selectedItem);
